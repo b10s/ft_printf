09ad2050cdf60ef86e7d6c0a5da9e9774b4874d8
Andrei Enshin <aenshin@c1r7s7.42tokyo.jp>
2024-08-03 23:44:46 +0900

    finish %x, %X

--- ft_printf.c
+++ ft_printf.c
@@ -6,9 +6,9 @@
 // d - int: DONE
 // i - int: DONE
 // u - int as unsigned int: DONE
-// x - ?
-// X - ?
-// % - % itself
+// x - int as unsigned in hex: DONE
+// X - int as unsigned in HEX: DONE 
+// % - % itself: DONE
 
 // bonus
 // 1) Manage any combination of the following flags: ’-0.’ and the field minimum widt under all conversions.
@@ -23,6 +23,47 @@
 #include <stdlib.h>
 
 static	short	int	ft_count_future_len(unsigned int n);
+static	short	int	ft_count_future_len_hx(unsigned int n);
+
+char *
+	ft_utoax(unsigned int n) {
+	unsigned long	x;
+	short int		len;
+	short int		i;
+	char			*res;
+	char *base = "0123456789abcdef";
+	/*
+	int x = c / 16;
+	int y = c % 16;
+	ft_putchar_fd(base[y], 1);
+	 */
+
+	x = n;
+	res = NULL;
+	// TODO: leading zero - check and calculate len or assign \0 by i not by len
+	len = ft_count_future_len_hx(n);
+	len = len*2;
+	res = malloc(len + 1);
+	if (res == NULL)
+		return (NULL);
+	i = len - 1;
+	while (i >= 0)
+	{
+		int b = x % 256;
+
+		int xx = b / 16;
+		int yy = b % 16;
+		res[i] = base[yy];
+		i--;
+		res[i] = base[xx];
+		i--;
+
+		x = x / 256;
+	}
+
+	res[len] = '\0';
+	return (res);
+}
 
 char *
 	ft_utoa(unsigned int n) {
@@ -47,11 +88,21 @@ char *
 	return (res);
 }
 
+// WHAT static means here in return value?
+// or it is function not value?
 static short int
-	ft_detect_sign(int n) {
-	if (n < 0)
+	ft_count_future_len_hx(unsigned int n) {
+	short int	res;
+
+	if (n == 0)
 		return (1);
-	return (0);
+	res = 0;
+	while (n != 0)
+	{
+		res++;
+		n = n / 256;
+	}
+	return (res);
 }
 
 static short int
@@ -84,6 +135,20 @@ print_hx(unsigned char c, int first) {
 }
 
 void
+print_big_hx(unsigned char c, int first) {
+	char *base = "0123456789ABCDEF";
+	int x = c / 16;
+	int y = c % 16;
+
+	if ( first == 1 && c < 16) {
+		ft_putchar_fd(base[y], 1);
+	} else {
+		ft_putchar_fd(base[x], 1);
+		ft_putchar_fd(base[y], 1);
+	}
+}
+
+void
 print_in_hex(char *p, short sz)
 {
 	//printf("((%p))", *p);
@@ -95,7 +160,7 @@ print_in_hex(char *p, short sz)
 			first = 1;
 		}
 		if (started == 1 ) {
-			printf("(%d)", p[i]);
+			//printf("(%d)", p[i]);
 			print_hx(p[i], first);
 		}
 		first = 0;
@@ -163,6 +228,38 @@ ft_printf(const char *fmt, ...)
 				free(str);
 				fmt++;
       } else if (*(fmt+1) == 'x') {
+				unsigned int x = va_arg(ap, int);
+				char *str = ft_utoax(x);
+				if (*str == '0') {
+        	ft_putstr_fd(str+1, fd);
+				} else {
+        	ft_putstr_fd(str, fd);
+				}
+				free(str);
+				fmt++;
+      } else if (*(fmt+1) == 'X') {
+				unsigned int x = va_arg(ap, int);
+				char *str = ft_utoax(x);
+				//TODO: check for null and in other places read code check for return values for malloc and other funcs
+				char *bigS = malloc(ft_strlen(str));
+				// TODO check for return value
+				ft_strlcpy(bigS, str, ft_strlen(str)+1);
+				char *tmp;
+				tmp = bigS;
+				char c;
+				//printf("s [%s]\n", tmp);
+				while(*tmp) {
+					c  = ft_toupper(*tmp);
+					*tmp = c;
+					tmp++;
+				}
+				if (*bigS == '0') {
+        	ft_putstr_fd(bigS+1, fd);
+				} else {
+        	ft_putstr_fd(bigS, fd);
+				}
+				free(str);
+				free(bigS);
 				fmt++;
 			// prints %% and unknown like %w
 			} else {
@@ -193,12 +290,15 @@ void main() {
 	int mo = -1;
 
   char *fmt = "c: [%c], str: [%s], void * [%p], digits [%d %d %d %d %d %d %d %d %d] u: [%u %u %u %u %u %u %u %u %u] %w and %%\n";
+	char *fmt2 = "x: [%x %x %x %x %x %x %x %x %x], X: [%X %X %X %X %X %X %X %X %X]\n";
 
 	printf("orig: ");
    printf(fmt, c, str, p, x, y, z, m, n, mm, nn, mo, umm, x, y, z, m, n, mm, nn, mo, umm);
+	 printf(fmt2, x, y, z, m, n, mm, nn, mo, umm, x, y, z, m, n, mm, nn, mo, umm);
 
-	ft_printf("my:   ");
+	ft_printf("\nmy:   ");
 ft_printf(fmt, c, str, p, x, y, z, m, n, mm, nn, mo, umm, x, y, z, m, n, mm, nn, mo, umm);
+ft_printf(fmt2, x, y, z, m, n, mm, nn, mo, umm, x, y, z, m, n, mm, nn, mo, umm);
 
-	printf("size of void * is: [%d]\n", sizeof(void *));
+	//printf("size of void * is: [%d]\n", sizeof(void *));
 }
